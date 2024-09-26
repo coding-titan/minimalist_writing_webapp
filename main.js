13ba054907254fda038472a960c10b4633bea5d4
Seb <sebastian.manieri@gmail.com>
2024-09-26 15:44:40 +0200

    collapse toggle now works

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 // Variables
 
 const collapseButton = document.getElementById("collapse-button");
-const navBar = document.querySelector("nav")
+const navBar = document.querySelector("nav");
+let isCollapsed = false;
+const collapseIcon = document.getElementById("collapse-icon");
+const expandIcon = document.getElementById("expand-icon")
 
-const collapse = function() {
-    document.body.style.backgroundColor = "green";
-    navBar.style.backgroundColor = "red";
+const toggleCollapse = function() {
+    // if statement to check if it is currently collapsed
+    if (isCollapsed === false) {
+        // reduce width of navbar so that only arrow fits
+        navBar.style.width = "3rem";
     
+        // Change to expand icon
+        collapseIcon.style.display = "none";
+        expandIcon.style.display = "block";
+
+        // Set collapse
+        isCollapsed = true;
+
+    } else {
+        // Change back width
+        navBar.style.width = "auto";
+
+        // Change back icon
+        collapseIcon.style.display = "block";
+        expandIcon.style.display = "none";
+
+        // Set collaps
+        isCollapsed = false;
+    }
 }
 
-document.addEventListener("click", collapse)
+document.addEventListener("click", toggleCollapse)
 
