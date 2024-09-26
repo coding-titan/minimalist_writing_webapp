3620e465f228413b9183e41ed6ee98ff8ed23a38
Seb <sebastian.manieri@gmail.com>
2024-09-26 15:56:48 +0200

    collapse is now smooth with hardcoded width

--- main.js
+++ main.js
@@ -9,9 +9,10 @@ const expandIcon = document.getElementById("expand-icon")
 const toggleCollapse = function() {
     // if statement to check if it is currently collapsed
     if (isCollapsed === false) {
-        // reduce width of navbar so that only arrow fits
+        // reduce width of navbar so that only arrow fits & reduce opacity
         navBar.style.width = "3rem";
-    
+        navBar.style.opacity = "0.5";
+
         // Change to expand icon
         collapseIcon.style.display = "none";
         expandIcon.style.display = "block";
@@ -20,17 +21,18 @@ const toggleCollapse = function() {
         isCollapsed = true;
 
     } else {
-        // Change back width
-        navBar.style.width = "auto";
+        // Change back width & opacity
+        navBar.style.width = "11rem";
+        navBar.style.opacity = "1";
 
         // Change back icon
         collapseIcon.style.display = "block";
         expandIcon.style.display = "none";
 
-        // Set collaps
+        // Set collapse
         isCollapsed = false;
     }
 }
 
-document.addEventListener("click", toggleCollapse)
+collapseButton.addEventListener("click", toggleCollapse)
 
